a640ed36255738061032e31f3b5c7a12d5dfa49d
Denys Dudnik <dudnik.gg@gmail.com>
2025-06-05 15:01:02 +0300

    :sparkles: Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: [".pnpm-store/**", "**/migrations/*"],
+}, {
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+      singleline: {
+        max: 2,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["warn", {
+      case: "camelCase",
+      ignore: ["README.md"],
+    }],
+  },
+}))
+  .override("antfu/vue/rules", {
+    rules: {
+      "vue/padding-line-between-tags": "error",
+      "vue/padding-lines-in-component-definition": "error",
+      "vue/padding-line-between-blocks": "error",
+      "vue/template-curly-spacing": "error",
+    },
+  });
